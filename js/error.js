db1506da26bdd145b9275fcfd1add6a53d32f58c
mim2041 <suraiyamim338@gmail.com>
2023-03-03 22:09:43 +0600

    nwebsite modified

---
+++ js/error.js
@@ -0,0 +1,12 @@
+console.log(1);
+setTimeout(() => {
+    console.log(2);
+}, 3000);
+console.log(3);
+
+const phones = {
+    data: 1,
+    property: {name:'iphone'}
+}
+
+phones.forEach(phone => console.log(phone.p))
